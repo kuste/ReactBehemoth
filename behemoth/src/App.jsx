19acc8a79d260d5c1941dc0897f6914613258096
Ivica Kustura <ivica.kustura@netlab.hr>
2019-02-19 10:28:37 +0100

    first commit

---
+++ behemoth/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Welcome to the </h2> <h1>BEHEMOTH</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
