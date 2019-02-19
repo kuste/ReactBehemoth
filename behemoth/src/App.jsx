37f4785bd5b9277cc811750bde909a7b11d92278
Sime Martinov <sime.martinov@rathmann.hr>
2019-02-19 10:31:43 +0100

    added extra description

--- behemoth/src/App.jsx
+++ behemoth/src/App.jsx
@@ -6,6 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <h2>Welcome to the </h2> <h1>BEHEMOTH</h1>
+        <h2>The very best app in the Balkans</h2>
       </div>
     );
   }
